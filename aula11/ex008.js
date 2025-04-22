134354526164796cbc52146a5f0070c79406dd20
Anna Paula <bueno.annarsa@gmail.com>
2025-04-22 13:25:33 -0300

    Exercícios sobre a aula de condições simples e compostas.

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,3 +1,6 @@
-var vel = 60.5;
-console.log(`A velocidade do seu carro é ${vel}Km/h`);
+var vel = 50;
+console.log(`A velocidade do seu carro é ${vel}Km/h.`);
+if (vel > 60) {
+  console.log(`Você ultrapassou a velocidade permitida. MULTADO!`);
+}
 console.log("Dirija seu carro sempre usando cinto de segurança!");

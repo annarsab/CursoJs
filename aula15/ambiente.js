7094d8039a38c2f36fe8641b2be1f2532895fb63
Anna Paula <bueno.annarsa@gmail.com>
2025-05-06 13:30:12 -0300

    Aula sobre estruturas de repetição.

---
+++ aula15/ambiente.js
@@ -0,0 +1,14 @@
+var c = 1;
+while (c <= 500) {
+  console.log(`Passo ${c}`);
+  c++;
+}
+
+/*
+console.log("Tudo bem?");
+console.log("Tudo bem?");
+console.log("Tudo bem?");
+console.log("Tudo bem?");
+console.log("Tudo bem?");
+console.log("Tudo bem?");
+*/

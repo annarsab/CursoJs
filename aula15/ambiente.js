b0db04f18d8aaa22fc2fa5bade47c71754364788
Anna Paula <bueno.annarsa@gmail.com>
2025-05-13 13:19:04 -0300

    Exercícios de repetição

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,14 +1,23 @@
-var c = 1;
-while (c <= 500) {
+//estrutura de repetição com teste lógico no final
+
+var c = 9;
+do {
+  console.log(`Passo ${c}`);
+  c++;
+} while (c <= 10);
+
+//estrutura de repetição com teste lógico no início
+var c = 9;
+while (c <= 10) {
   console.log(`Passo ${c}`);
   c++;
 }
 
-/*
+//estrutura que também é possível mas não é uma prática legal
+
 console.log("Tudo bem?");
 console.log("Tudo bem?");
 console.log("Tudo bem?");
 console.log("Tudo bem?");
 console.log("Tudo bem?");
 console.log("Tudo bem?");
-*/

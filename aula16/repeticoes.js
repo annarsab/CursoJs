ea60f86c7437913e42b1bb15a15234946ba5c6b4
Anna Paula <bueno.annarsa@gmail.com>
2025-05-14 13:19:44 -0300

    Aprendendo mais sobre For e também sobre modo de depuração

--- aula16/repeticoes.js
+++ aula16/repeticoes.js
@@ -1,1 +1,19 @@
-for (var c = 1; c <= 10; c++) {}
+/*
+  var c = 1
+  while (c <= 5){
+      console.log(c)
+      c++
+  }
+
+  do {
+       console.log(c)
+       c++
+  } while (c <= 5)
+*/
+
+//------------------------
+console.log("Vai começar...");
+for (var c = 1; c <= 4; c++) {
+  console.log(c);
+}
+console.log("FIM!");
